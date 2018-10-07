2065451af819f8aba3cbcb67839529f145c6252f
Dmitriy Kunets <t38@tut.by>
2018-10-07 16:09:52 +0300

    the task was completed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (propertyName) => {
+  return propertyName
+};
+
+const createNotEnumerableProperty = (propertyName) => {};
+
+const createProtoMagicObject = () => {
+  let magic = () => {};
+  magic.prototype = magic.__proto__;
+  return magic;
+};
+
+const incrementor = (() => {
+  let counter = 0;
+  const fc = () => {
+    counter++;
+    return fc;
+  };
+
+  fc.valueOf = () => counter;
+  return fc;
+})();
+
+const asyncIncrementor = (() => {
+  let counter = 0;
+  const fc = () => {
+    counter++;
+    return new Promise((resolve, reject) => resolve(fc));
+  };
+
+  fc.valueOf = () => counter;
+  return fc;
+})();
+
+const createIncrementer = function* () {
+  let counter = 1;
+  while (true) {
+    yield counter++;
+  }
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (param) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => { resolve(param) }, 1001)
+  })
+};
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+  return arr.sort((a, b) => a.__proto__ - b.__proto__);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
